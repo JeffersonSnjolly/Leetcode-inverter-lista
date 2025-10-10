2ed9f4bd18c70e039fa0c769a89f713ec4620bce
Jefferson Santos Silva <84148608+JeffersonSnjolly@users.noreply.github.com>
2025-10-10 16:56:58 -0300

    Update comandos_pytho.py

--- comandos_pytho.py
+++ comandos_pytho.py
@@ -265,3 +265,202 @@ print(lingua)
 #podemos usar o sorted() função built-in do python
 lingua = ['python','c','java','c#','ruby','javaScript','cobol']
 sorted(lingua, key=lambda x:len(x), reverse=True)
+
+
+#funçõs
+
+def exibir_mensagem():
+    print("Olá mundo")
+    
+def exibir_mensagem_2(nome):
+    print(f"Olá {nome}")
+
+def exibir_mensagem_3(nome = "Tamyres"):
+    print(f"Olá {nome}")
+
+
+#chamando as funções
+exibir_mensagem()
+#Argumento obrigatório 
+exibir_mensagem_2("jeff")
+#Vem como o nome Tamyres como padrão
+exibir_mensagem_3()
+#podemos alterarna chamada
+exibir_mensagem_3(nome='Maria')
+
+# O podemos retorna um None ou multiplos valores
+def soma(*numeros):
+    return sum(numeros)
+
+def ante_e_sucess(numero):
+    ante = numero -1
+    sucess = numero + 1
+    return ante, sucess
+def func_03():
+    print("olá mundo")
+
+print(soma(1,2,3,4,5,6,7,8,9))
+print(ante_e_sucess(10))
+print(func_03())
+
+#Argumentos Nomeados
+
+def carros_01(marca,modelo,ano,placa):
+    
+    print(f"Carro inserido com sucesso! Marca:{marca} / Modelo:{modelo} / Ano:{ano} / Placa:{placa}")
+
+#possibilidades para iserir os dados
+#sem nomear
+carros_01("Fiat",'Fast Back',2025,'abc-1234')
+
+#nomeados
+carros_01(marca="BMW",modelo="X6 serie-M",ano=2024,placa='cde-6666')
+
+#Passando como um dicionario
+carros_01(**{"marca":"Porche","modelo":"911 Carreira GT","ano":2022,"placa":"sos-9874"})
+
+#*args e **kargs
+#args retorna um tupla
+#**kargs retorna um dicionario
+
+def exibir_poema(data_extenso, *args, **kargs):
+    texto = "\n".join(args) 
+    meta_dados = "\n".join([f"{chave.upper()}: {valor}" for chave,valor in kargs.items()])
+    mensagem = f"{data_extenso}\n\n{texto}\n\n{meta_dados}"
+    return mensagem
+
+#Declarando o peoma
+poema = exibir_poema(
+    "Sexta feira 10 de Outubro, São paulo",
+    "Rosas são belas.",
+    "E o sangue, escorre delas.",
+    "Rosas são belas.",
+    "E na morte, recebe elas.",
+    Autor="Jefferson",Ano=2025
+)
+
+# Chamando o poema
+print(poema)
+
+
+# Paramentros especiais
+# Obrigando a passar nomeados ou não usando o /
+# Usando o / fica opcional
+
+def carros3(modelo,marca,/,ano,placa):
+    mensagem = f"{modelo}, {marca}, {ano}, {placa}"
+    return mensagem
+
+#Válido
+car1 = carros3('palio','fiat',ano=2010,placa='abs-2233')# Correto, os dois primeiros paramentros não devem ser nomeados
+print(car1)
+
+# Inválido
+car2 = carros3(modelo='palio',marca='fiat',ano=2010,placa='abs-2233') # Gera um erro, os dois primeiros paramentros não devem ser nomeados
+print(car2)
+
+# Usando * todos os valores são obrigatorios de serem nomeados
+def carros4(*,modelo,marca,ano,placa):
+    mensagem = f"{modelo}, {marca}, {ano}, {placa}"
+    return mensagem
+
+# Inválido
+car3 = carros4('palio','fiat',ano=2010,placa='abs-2233') # Gera um erro, os dois primeiros paramentros não devem ser nomeados
+print(car1)
+
+#Válido 
+car4 = carros4(modelo='palio',marca='fiat',ano=2010,placa='abs-2233') # Correto, os dois primeiros paramentros não devem ser nomeados
+print(car4)
+
+# Hibridos com / e *
+def carros5(modelo,marca,/,ano,*,placa,tipo):
+    mensagem = f"{modelo}, {marca}, {ano}, {placa}, {tipo}"
+    return mensagem
+
+car5= carros5('gol','volkswagen',2012,placa='dgf-4455',tipo='Etanol')
+print(car5)
+
+
+# Objetos de primeira classe
+# passando a função como objeto de primeira classe
+
+def somar(a,b):
+    soma = a + b
+    return soma
+
+def subtrair(a,b):
+    sub = a - b
+    return sub
+
+def nova_func(a,b, funcao):
+    resultado = funcao(a,b)
+    return f" o Resultado de {a} e {b} = {resultado}"
+
+a1 = nova_func(10,5,somar) # aqui a função somar não recebe os paranteses (), é usada como objeto de primeira classe
+print(a1)
+# Assim podemos a nova_func com resultados diferentes dependendo do objeto de primeira classe no passado
+a2 = nova_func(5,2,subtrair)
+print(a2)
+
+#podemos atribuir esse comportamento para uma variavel
+op = somar
+print(op(5,2))
+
+
+# Escopo local e global
+# Utilize a palavra global apesar de não ser uma boa prática
+
+# exemplo 01
+salario = 2000
+def bonus(valor):
+    global salario
+    salario += valor
+    return salario
+
+
+print(salario)
+
+bonus(500)
+
+print(salario)
+
+
+
+# exemplo 02
+nome = "Jeff"
+
+def novo_nome():
+    nome = "Lucas"
+    print(nome)
+    
+print(nome)
+
+novo_nome()
+
+
+#Exemplo 03
+#listas são sempre interaiveis imutaveis por padrão altera no global mesmo não passando a palavra reservada global
+lista = [1]
+
+def alterando_lista(lista):
+    lista.append(2)
+    lista.append(3)
+    
+
+alterando_lista(lista)
+
+print(lista)
+
+#Exemplo 04
+
+
+def alterando_lista2(lista):
+    lista2 = lista.copy()
+    lista2.append(2)
+    lista2.append(3)
+    
+
+alterando_lista(lista)
+
+alterando_lista2(lista)
+
